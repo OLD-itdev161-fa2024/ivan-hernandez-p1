5e8cb1e31919a482d7c1a4af9b30549c6f76ea6f
hernai25 <hernai25@gmatc.matc.edu>
2023-10-05 05:12:08 -0500

    Mongo

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import connectDatabase from './config/db';
+const router = express.Router();
+const Product = require('./products.json');
 
 const app = express();
 
@@ -12,5 +14,15 @@ app.get('/', (req, res) =>
     res.send('Hello')
 );
 
+router.get('/products', async (req, res) => {
+    try {
+      const products = await Product.find();
+      res.json(products);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+
+  module.exports = router;
 
 app.listen(3000, () => console.log(`Express server running on port 3000`));
